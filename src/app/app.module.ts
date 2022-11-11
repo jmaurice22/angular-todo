0cdfc8113ff8445529ad1f7a9ec13df8440aea4d
jmaurice22 <57735087+jmaurice22@users.noreply.github.com>
2022-11-10 20:20:20 -0700

    finished todo form layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+
 
 
 @NgModule({
@@ -11,10 +13,13 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
     AppComponent,
     TodoComponent,
     TodoListComponent,
+    TodoFormComponent,
+    
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
